3c0e45c0b4c07702b22bdd3fb9b9761f915adf1c
Mehmet Sait TAYŞİ <250541074@firat.edu.tr>
2025-12-03 19:41:25 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,10 +11,22 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
-        getNumber();
+        getNumber();Metot calisir, 42 degerini uretir ancak bu deger hicbir degiskene
+         atanmadigi veya kullanilmadigi icin hemen atilir goz ardi edilir.
+        Program herhangi bir hata vermeden calismaya devam eder.
+
+
+        
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
-        // System.out.println(sayHello() + 7);
+        // System.out.println(sayHello() + 7); Bu satir derleme hatasina neden olur.
+         sayHello() metodu void hicbir deger dondurur. Java, void bir degeri
+         +7 gibi bir ifade icinde kullanmaya calisirken bir tur (type) hatasi verir.
+        Cunku 'void' bir degere sahip degildir.
+
+
+
+            
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
