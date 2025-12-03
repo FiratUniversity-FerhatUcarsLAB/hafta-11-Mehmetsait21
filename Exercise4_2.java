685be07fee9aab92db670e190e8f1b5c3d913e8e
Mehmet Sait TAYŞİ <250541074@firat.edu.tr>
2025-12-03 19:17:40 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -2,22 +2,31 @@ public class Exercise4_2 {
 
   // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 
-    public static void main(String[] args) {
-        zippo("rattle", 13);
+    public static void main(String[] args) { bu satır çalışmaz 
+        zippo("rattle", 13); bırınci satır bu olacak zippo metodu çagrılır satır 10 a atlar 
     }
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+    public static void baffle(String blimp) { tanımlama satırı çaşlışmaz 
+        System.out.println(blimp); if blogu çalışmadı 
+        zippo("ping", -5); zippo metodu çagrılır satır 10 a atlar
     }
 
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+    public static void zippo(String quince, int flag) { tanımlama satırı çaşlışmaz 
+        if (flag < 0) { yanlış false 10. satıra atlar 
+            System.out.println(quince + " zoop"); if blogu çalışmadı 
+        } else { else blogu çalışmadı 
+            System.out.println("ik"); ekrana ik yazar 
+            baffle(quince); baffle metodu çagrılır satır 5 e atlar
+            System.out.println("boo-wa-ha-ha"); Ekrana: boo - wa - ha - ha. baffle metodundan sonra geri dönüldü
         }
     }
-}
+}2	zippo ( " rattle " , 13) ;	1
+3	}	12
+6	System . out . println ( blimp ) ;	5
+7	zippo ( " ping " , -5) ;	6
+8	}	10
+11	if ( flag < 0) {	2, 7
+12	System . out . println ( quince + " zoop " ) ;	8
+14	System . out . println ( " ik " ) ;	3
+15	baffle ( quince ) ;	4
+16	System . out . println ( " boo - wa - ha - ha " ) ;	11
