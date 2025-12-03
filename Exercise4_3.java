5e394f44bcae9bd2b886ba45b19bcd5138536d1d
Mehmet Sait TAYŞİ <250541074@firat.edu.tr>
2025-12-03 19:35:34 +0300

    Update Exercise4_3.java

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -22,3 +22,31 @@ public class Exercise4_3 {
         System.out.println(".");
     }
 }
+Satır No,Metot,Komut,Çıktı Akışı
+7,main,(Başlangıç),
+8,main,"System.out.print ( "" No , I "" ) ;","No, I"
+9,main,zoop () ; → Satır 2,
+2,zoop,baffle () ; → Satır 15,
+15,baffle,"System.out.print ( "" wug "" ) ;","No, I wug"
+16,baffle,ping () ; → Satır 20,
+20,ping,"System.out.println ( "" . "" ) ;","No, I wug . (Satır sonu)"
+17,baffle,(Bitti) → Satır 3,
+3,zoop,"System.out.print ( "" You wugga "" ) ;",You wugga
+4,zoop,baffle () ; → Satır 15,
+15,baffle,"System.out.print ( "" wug "" ) ;",You wugga wug
+16,baffle,ping () ; → Satır 20,
+20,ping,"System.out.println ( "" . "" ) ;",You wugga wug . (Satır sonu)
+17,baffle,(Bitti) → Satır 5,
+5,zoop,(Bitti) → Satır 10,
+10,main,"System.out.print ( "" I "" ) ;",I
+11,main,baffle () ; → Satır 15,
+15,baffle,"System.out.print ( "" wug "" ) ;",I wug
+16,baffle,ping () ; → Satır 20,
+20,ping,"System.out.println ( "" . "" ) ;",I wug . (Satır sonu)
+17,baffle,(Bitti) → Satır 12,
+12,main,(Bitti),
+
+cıktı 
+    No, I wug .
+You wugga wug .
+I wug .
