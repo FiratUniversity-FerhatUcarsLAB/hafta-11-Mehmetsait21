5fc48a6dc51a24613943e12ffa5f9a0209a06823
Mehmet Sait TAYŞİ <250541074@firat.edu.tr>
2025-12-03 18:09:33 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,34 @@
-public class Exercise4_1 {
 
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+public class SimpleDateFormatter {
+
+   
+    public static void printAmerican(String day, String month, int date, int year) {
+        
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+
+    public static void printEuropean(String day, String month, int date, int year) {
+        
+        System.out.println(date + " " + month + " " + year + ", " + day + ".");
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        String day = "Monday";
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        System.out.println("Amerikan Formatı:");
+        printAmerican(day, month, date, year);
+
+        System.out.println("Avrupa Formatı:");
+        printEuropean(day, month, date, year);
     }
 }
+cıktı 
+    merikan Format?:
+Monday, July 22, 2019
+
+Avrupa Format?:
+22 July 2019, Monday.
